23e5c085633c0ef87911ccc5659a509904a624e3
takumiozato <dolphin.rokujiro@gmail.com>
2020-08-03 10:03:41 +0900

    responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    className="linkBar-list"
-    style={{
-      backgroundColor: `#1A237E`,
-      padding: "6px 12px 10px",
-      height: "60px",
-      boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        color: `white`,
-        textDecoration: `none`,
-      }}
-    >
-      <span
-        style={{
-          fontSize: "10px",
-        }}
-      >
-        ONLINE BOOK STORE
-      </span>
-      <h1
-        style={{
-          fontSize: "20px",
-          fontWeight: "bold",
-          lineHeight: "1",
-        }}
-      >
-        {siteTitle}
-      </h1>
+  <header className={styles.header}>
+    <Link to="/" className={styles.headerLink}>
+      <span className={styles.headerSubTitle}>ONLINE BOOK STORE</span>
+      <h1 className={styles.headerTitle}>{siteTitle}</h1>
     </Link>
   </header>
 )
