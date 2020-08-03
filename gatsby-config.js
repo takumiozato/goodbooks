30468cd11d04f336b1361f8e2f74b28ebbc6c4f6
takumiozato <dolphin.rokujiro@gmail.com>
2020-08-03 09:34:11 +0900

    add google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,16 @@ module.exports = {
         endpoint: "articles",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Noto Sans JP`,
+          `Roboto\:400,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
 import LinkBar from "./linkBar"
 import "./layout.css"
