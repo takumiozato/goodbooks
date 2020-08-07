b005bcb57b84d50b78a9a9acb82f78bcf9e5606c
takumiozato <dolphin.rokujiro@gmail.com>
2020-08-07 16:04:42 +0900

    fix style

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,23 @@ const Header = ({ siteTitle }) => (
         <span className={styles.headerSubTitle}>ONLINE BOOK STORE</span>
         <h1 className={styles.headerTitle}>{siteTitle}</h1>
       </Link>
+      <ul className={styles.headerNavList}>
+        <li className={styles.headerNavItem}>
+          <Link to="/recommend/" className={styles.headerNavLink}>
+            オススメ特集一覧
+          </Link>
+        </li>
+        <li className={styles.headerNavItem}>
+          <Link to="/sale/" className={styles.headerNavLink}>
+            セール情報一覧
+          </Link>
+        </li>
+        <li className={styles.headerNavItem}>
+          <Link to="/ranking/" className={styles.headerNavLink}>
+            売れ筋ランキング一覧
+          </Link>
+        </li>
+      </ul>
     </div>
   </header>
 )
