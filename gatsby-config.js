b113d13adcd565829a152b0c533eedce6caddf19
takumiozato <dolphin.rokujiro@gmail.com>
2020-08-09 15:21:30 +0900

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     author: `@goodbooks`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
